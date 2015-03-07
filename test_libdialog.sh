8727ef193e2d64e1dad03b2fe156d22b324b94a7
Ricardo Biehl Pasquali <rbpoficial@gmail.com>
2015-03-07 14:45:28 -0300

    Added some tests.
    Added some more tests.

--- test_libdialog.sh
+++ test_libdialog.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#define_interface shell
 get "Put your name:" "" "Can you put your name in the text box?"
 name="$retvar"
 yesno "Your name is $name?"
@@ -7,4 +7,19 @@ if [ $retvar = 0 ]; then
   put "Name defined as $name" 0
 else
   put "Name isn't defined" 3
+  exit
 fi
+
+menu "How print" <<EOF
+"orange" "Print in orange `cecho orange "$name"`"
+"blue" "Print in blue `cecho darkblue "$name"`"
+EOF
+
+case "$retvar" in
+  0)
+    cecho orange "\n$name\n";;
+  1)
+    cecho darkblue "\n$name\n";;
+esac
+
+exit
